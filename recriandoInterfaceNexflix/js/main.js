570a348a1ed99dd8b5a4082830ee296b3f6533e7
Irineu Marcelo Maioli <79086470+irineumarcelomaioli@users.noreply.github.com>
2022-01-10 23:09:24 -0300

    Add files via upload

---
+++ recriandoInterfaceNexflix/js/main.js
@@ -0,0 +1,10 @@
+function assistirAgora() {
+  window.location.href = 'https://www.youtube.com/watch?v=ORaEE8h3BzY';
+
+  return;
+}
+
+function maisInformacoes() {
+  window.open('https://www.tecmundo.com.br/minha-serie/231152-livro-boba-fett-primeiro-episodio-estreia-disney.htm#:~:text=A%20s%C3%A9rie%20dever%C3%A1%20mostrar%20%E2%80%9Co,%E2%80%9D%2C%20segundo%20a%20sinopse%20oficial.');
+  return;
+}
